46dbd4ec2c5cc2a821a8928a55b700ae0c73f5d0
Shubham <Ks6422@hotmail.com>
2020-09-11 14:12:59 +0530

    loader background color changed

--- src/components/Loader/Loader.component.jsx
+++ src/components/Loader/Loader.component.jsx
@@ -4,9 +4,11 @@ import "./loader.style.scss";
 function Loader() {
   return (
     <div className="loader">
-      <div className="lds-hourglass"></div>
-      <div>
-        <p>Pofile is being loding...</p>
+      <div className="loader-background">
+        <div className="lds-hourglass"></div>
+        <div>
+          <p>Pofile is being loding...</p>
+        </div>
       </div>
     </div>
   );
